79f0506c0ecb80cb69ca2c2b640e1c4f5ebeaf18
Márk Bartha <barthamark@gmail.com>
2025-11-01 20:34:52 +0100

    Adding new MAUI Blazor Hybrid app

---
+++ src/Frontend/MarkBartha.HarvestDemo.App.Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MarkBartha.HarvestDemo.App.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "MarkBartha.HarvestDemo.App.Maui" };
+    }
+}
---
+++ src/Frontend/MarkBartha.HarvestDemo.App.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MarkBartha.HarvestDemo.App.Maui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
